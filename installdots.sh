9e65db8eda4fa3e72e2e19058c5d7d1c10bd2a43
Bilgehan Kaya <bilbat35@gmail.com>
2021-03-14 00:03:01 +0300

    auto commit

--- installdots.sh
+++ installdots.sh
@@ -20,7 +20,7 @@ sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
 # Refresh keyrings
 pacman --noconfirm -Sy archlinux-keyring
 
-aur_helper="paru"
+aur_helper="yay"
 # AUR helper install
 pacman --noconfirm --needed -S git curl base-devel
 pacman -Qq | grep -q "$aur_helper" ||
